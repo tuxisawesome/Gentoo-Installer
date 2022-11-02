75bf939dc166b9ddf19ef648874ef6ef78aca2ed
tuxisawesome <walter.brobson@gmail.com>
2022-11-01 21:35:44 -0400

    testing
    testing

--- scripts/3-makeconf.sh
+++ scripts/3-makeconf.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-nano /mnt/gentoo/etc/portage/make.conf
+
+display_result() {
+  dialog --title "$1" \
+    --no-collapse \
+    --msgbox "$result" 0 0
+}
+result="A generic make.conf has been configured for you. Make as many modifications as you want."
+display_result "Info"
+cp generic-configs/make.conf /mnt/gentoo/etc/portage/make.conf
--- test.sh
+++ test.sh
@@ -1,1 +1,36 @@
-exit
+display_result() {
+  dialog --title "$1" \
+    --no-collapse \
+    --msgbox "$result" 0 0
+}
+
+
+    DISK="/dev/sda"
+ESP="/dev/sda1"
+BOOT="/dev/sda2"
+ROOT="/dev/sda3"  
+NAME="HI"
+      
+      
+      DISK=$(\
+        dialog --backtitle $NAME --title "Main drive name" \
+                --inputbox "Enter the drive name:" 8 40 \
+        3>&1 1>&2 2>&3 3>&- \
+        )
+      ESP=$(\
+        dialog --backtitle $NAME --title "EFI Partition Name" \
+                --inputbox "Enter the EFI Partition name (try ${DISK}1):" 8 40 \
+        3>&1 1>&2 2>&3 3>&- \
+        )
+        BOOT=$(\
+        dialog --backtitle $NAME --title "Boot Partition Name" \
+                --inputbox "Enter the Boot Partition name (try ${DISK}2):" 8 40 \
+        3>&1 1>&2 2>&3 3>&- \
+        )
+        ROOT=$(\
+        dialog --backtitle $NAME --title "Root Partition Name" \
+                --inputbox "Enter the Root Partition name (try ${DISK}3):" 8 40 \
+        3>&1 1>&2 2>&3 3>&- \
+        )
+        result="Main Drive: ${DISK}\nEFI: ${ESP}\nBoot: ${BOOT}\nRoot: ${ROOT}"
+        display_result "Info"
