629e03265345a121ea1b6bfc64f1770b6aec5127
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2023-12-08 16:33:20 -0500

    Update 1-diskpart.sh

--- scripts/1-diskpart.sh
+++ scripts/1-diskpart.sh
@@ -1,10 +1,32 @@
 user_input=$(\
-  dialog --backtitle "Walter's Gentoo Installer" --title "Drive name" \
+  dialog --backtitle "Walter's Gentoo Installer" --title "Drive Setup" \
          --inputbox "Enter the drive name:" 8 40 \
   3>&1 1>&2 2>&3 3>&- \
 )
+swap_input=$(\
+  dialog --backtitle "Walter's Gentoo Installer" --title "Drive Setup" \
+         --inputbox "Enter your swap amount:" 8 40 \
+  3>&1 1>&2 2>&3 3>&- \
+)
 
+total_swap=$(($swap_input + 131))
 
+clear
+echo "Please type these commands as follows:"
+echo "If you mess up, type 'mklabel gpt' to reset."
+echo ""
+echo "mklabel gpt"
+echo "unit mib"
+echo "mkpart primary 1 3"
+echo "name 1 grub"
+echo "set 1 bios_grub on"
+echo "mkpart primary 3 131"
+echo "name 2 boot"
+echo "set 2 boot on"
+echo mkpart primary 131 $total_swap
+echo "name 3 swap"
+echo mkpart primary $total_swap -1
+echo name 4 rootfs
 
 parted -a optimal $user_input
 
